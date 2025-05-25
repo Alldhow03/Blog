4488dff7ffdc723e5cc552135cc7b3f773c21e68
Alldhow <rhonaldhof@gmail.com>
2025-05-25 18:32:04 +0700

    penambahan fungsi update, create dan validasi update

--- app/Http/Controllers/Member/BlogController.php
+++ app/Http/Controllers/Member/BlogController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Member;
 
-use App\Http\Controllers\Controller;
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class BlogController extends Controller
 {
@@ -13,8 +15,9 @@ class BlogController extends Controller
      */
     public function index()
     {
-        $data = Post::all();
-        print_r($data);
+        $user = Auth::user();
+        $data = Post::where("user_id", $user->id)->orderBy('id', 'desc')->paginate(3);
+        return view('member.blogs.index', compact('data'));
     }
 
     /**
@@ -22,7 +25,7 @@ class BlogController extends Controller
      */
     public function create()
     {
-        //
+        return view('member.blogs.create');
     }
 
     /**
@@ -42,11 +45,12 @@ class BlogController extends Controller
     }
 
     /**
-     * Show the form for editing the specified resource.
+     * Show the form for editing the specified resource. 
      */
     public function edit(Post $post)
     {
-        //
+        $data = $post;
+        return view('member.blogs.edit', compact('data'));
     }
 
     /**
@@ -54,7 +58,41 @@ class BlogController extends Controller
      */
     public function update(Request $request, Post $post)
     {
-        //
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'content' => 'required',
+            'thumbnail' => 'image|mimes:jpg,jpeg,png|max:10240',
+        ], [
+            'title.required' => 'Judul harus diisi !',
+            'description.required' => 'Deskripsi harus diisi !',
+            'content.required' => 'Content harus diisi !',
+            'thumbnail.image' => 'Thumbnail harus diisi !',
+            'thumbnail.mimes' => 'Ekstensi yang diperbolehkan hanya JPG, JPEG & PNG !',
+            'thumbnail.max' => 'Ukuran gambar Max 10mb !',
+        ]);
+
+        if ($request->hasFile('thumbnail')) {
+            if (isset($post->thumbnail) && file_exists(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')) . "/" . $post->thumbnail)) {
+                unlink(public_path(getenv('CUSTOM_THUMBNAIL_LOCATION')) . "/" . $post->thumbnail);
+            }
+            $image = $request->file('thumbnail');
+            $image_name = time() . "_" . $image->getClientOriginalName();
+            $destination_path = public_path(getenv('CUSTOM_THUMBNAIL_LOCATION'));
+            $image->move($destination_path, $image_name);
+        }
+
+        $data = [
+            'title' => $request->title,
+            'description' => $request->description,
+            'thumbnail' => isset($image_name) ? $image_name : $post->thumbnail,
+            'content' => $request->content,
+            'status' => $request->status,
+            'slug' => $this->generateSlug($request->title, $post->id),
+        ];
+
+        post::where('id', $post->id)->update($data);
+        return redirect()->route('member.blogs.index')->with('success', 'Perubahan data berhasil disimpan');
     }
 
     /**
@@ -63,5 +101,18 @@ class BlogController extends Controller
     public function destroy(Post $post)
     {
         //
+    }
+
+    private function generateSlug($title, $id)
+    {
+        $slug = Str::slug($title);
+        $count = Post::where('slug', $slug)->when($id, function ($query, $id) {
+            return $query->where('id', '!=', $id);
+        })->count();
+
+        if ($count > 0) {
+            $slug = $slug . "-" . ($count + 1);
+        }
+        return $slug;
     }
 }
