dc5dff790544a077abbfbc2e543b4af0572870cc
Alldhow <rhonaldhof@gmail.com>
2025-06-19 11:27:32 +0700

    penambahan page blog-detail serta controllernya

--- app/Http/Controllers/Front/BlogDetailController.php
+++ app/Http/Controllers/Front/BlogDetailController.php
@@ -4,11 +4,26 @@ namespace App\Http\Controllers\Front;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class BlogDetailController extends Controller
 {
     public function detail($slug)
     {
-        echo $slug;
+        $data = Post::where('status', 'publish')->where('slug', $slug)->firstOrFail();
+        $pagination = $this->pagination($data->id);
+        return view('components.front.blog-detail', compact('data', 'pagination'));
+    }
+
+    private function pagination($id)
+    {
+        $dataPrev = Post::where('status', 'publish')->where('id', '<', $id)->OrderBy('id', 'desc')->first();
+        $dataNext = Post::where('status', 'publish')->where('id', '>', $id)->OrderBy('id', 'desc')->first();
+
+        $data = [
+            'prev' => $dataPrev,
+            'next' => $dataNext
+        ];
+        return $data;
     }
 }
